7a2363c90682f14a3710437345183ee032f8959f
tsukuruhito <ts.createlink@gmail.com>
2022-08-23 11:26:15 +0900

    test

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	extends: ['next/core-web-vitals', 'prettier'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
