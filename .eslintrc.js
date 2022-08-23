45898fb6164adf4b2231c644e03bd4ce70207f06
tsukuruhito <ts.createlink@gmail.com>
2022-08-23 15:46:19 +0900

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,39 @@ module.exports = {
 	rules: {
 		"react/react-in-jsx-scope": "off",
 	},
+	plugins: ["import", "unused-imports"],
 	settings: {
 		react: {
 			version: "detect",
 		},
+	},
+	rules: {
+		"import/order": [
+			"error",
+			{
+				groups: [
+					["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+				],
+				"newlines-between": "always",
+				pathGroupsExcludedImportTypes: ["builtin"],
+				alphabetize: {
+					order: "asc",
+					caseInsensitive: true,
+				},
+				pathGroups: [
+					{
+						pattern: "@/components/common",
+						group: "internal",
+						position: "before",
+					},
+					{
+						pattern: "@/components/hooks",
+						group: "internal",
+						position: "before",
+					},
+				],
+			},
+		],
+		"unused-imports/no-unused-imports": "error",
 	},
 };
