6debf8822477b333526d2ae1ee55f6aad84304c9
tsukuruhito <ts.createlink@gmail.com>
2022-08-23 12:09:41 +0900

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-	extends: ['next/core-web-vitals', 'prettier'],
+	extends: ["next/core-web-vitals", "prettier"],
 	rules: {
-		'react/react-in-jsx-scope': 'off',
+		"react/react-in-jsx-scope": "off",
 	},
 	settings: {
 		react: {
-			version: 'detect',
+			version: "detect",
 		},
 	},
 };
--- .lint-staged.config.js
+++ .lint-staged.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	'**/*.(ts|tsx)': () => 'tsc --pretty --noEmit',
+	"**/*.(ts|tsx)": () => "tsc --pretty --noEmit",
 
-	'**/*.(ts|tsx|js)': (filenames) => [
-		`eslint --fix ${filenames.join(' ')}`,
-		`prettier --write ${filenames.join(' ')}`,
+	"**/*.(ts|tsx|js)": (filenames) => [
+		`eslint --fix ${filenames.join(" ")}`,
+		`prettier --write ${filenames.join(" ")}`,
 	],
 
-	'**/*.(md|json)': (filenames) => `prettier --write ${filenames.join(' ')}`,
+	"**/*.(md|json)": (filenames) => `prettier --write ${filenames.join(" ")}`,
 };
