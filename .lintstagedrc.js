d1dbad76fa6172c89220290ee1c7dcaa248a4a9f
tsukuruhito <ts.createlink@gmail.com>
2022-08-23 12:23:58 +0900

    test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,10 @@
-import { relative } from 'path'
+import { relative } from "path";
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => relative(process.cwd(), f))
-    .join(' --file ')}`
+	`next lint --fix --file ${filenames.map((f) => relative(process.cwd(), f)).join(" --file ")}`;
 
 export default {
 	"**/*.(ts|tsx)": () => "tsc --pretty --noEmit",
-	'*.{js,jsx,ts,tsx}': [buildEslintCommand],
-	"**/*.(ts|tsx|js)": (filenames) => `prettier --write ${filenames.join(" ")}`
-}
+	"*.{js,jsx,ts,tsx}": [buildEslintCommand],
+	"**/*.(ts|tsx|js)": (filenames) => `prettier --write ${filenames.join(" ")}`,
+};
