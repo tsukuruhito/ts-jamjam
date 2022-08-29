b48be599c55f9143b57ad8317805f6d670f2df39
tsukuruhito <ts.createlink@gmail.com>
2022-08-29 22:05:29 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-module.exports = withPWA({
-	pwa: {
-		dest: "public",
-		register: true,
-		skipWaiting: true,
-		runtimeCaching,
-	},
+const NextConfig = {
 	reactStrinctMode: true,
 	swcMinify: true,
 	images: {
 		domains: ["images.microcms-assets.io"],
 	},
+};
+
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
 });
+
+module.exports = withPWA(NextConfig);
