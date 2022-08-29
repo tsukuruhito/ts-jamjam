8392e9b321e494563091f9dcad977d11ae686da9
tsukuruhito <ts.createlink@gmail.com>
2022-08-29 22:14:19 +0900

    repush

--- src/pages/_offline.tsx
+++ src/pages/_offline.tsx
@@ -1,3 +1,13 @@
-import Page from ".";
+import Head from 'next/head'
 
-export default Page;
+const Fallback = () => (
+  <>
+    <Head>
+      <title>next-pwa example</title>
+    </Head>
+    <h1>This is offline fallback page</h1>
+    <h2>When offline, any page route will fallback to this page</h2>
+  </>
+)
+
+export default Fallback
