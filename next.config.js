972c92e8d1ae126a9e4543dfdf5a338caecb16ab
tsukuruhito <ts.createlink@gmail.com>
2022-08-29 19:36:20 +0900

    change config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ["images.microcms-assets.io"],
+	},
+};
+
+module.exports = nextConfig;
 
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
@@ -7,10 +16,5 @@ module.exports = withPWA({
 	pwa: {
 		dest: "public",
 		runtimeCaching,
-	},
-	reactStrictMode: true,
-	swcMinify: true,
-	images: {
-		domains: ["images.microcms-assets.io"],
 	},
 });
